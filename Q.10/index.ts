2b8f3e7145c2033b72138fcf2cd95c3a62e1fbcf
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-05-16 20:44:43 +0500

    45 Assignments

---
+++ Q.10/index.ts
@@ -0,0 +1,15 @@
+//Q.10) Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+console.log(5 + 3); // Addition: 5 + 3 = 8
+console.log(10 - 2); // Subtraction: 10 - 2 = 8
+console.log(4 * 2); // Multiplication: 4 * 2 = 8
+console.log(16 / 2); // Division: 16 / 2 = 8
+
+const favoriteNumber: number = 8; // Storing my favorite number
+console.log(`My favorite number is ${favoriteNumber}.`); // Printing message revealing my favorite number
+
+
+
+
+
