50ae0219516fcd83787c419b841b2c0a38911519
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-05-16 20:43:06 +0500

    45 Assignments

---
+++ Q.5/index.ts
@@ -0,0 +1,6 @@
+// Q5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousperson: string="MUHAMMAD PEACE BE UPON HIM";
+let message: string="Seek knowledge from cradle to the grave"
+
+console.log(`${famousperson} once said, ${message}`)
